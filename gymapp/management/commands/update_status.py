44512c0e577d1275cb444064360638a2513d9fdb
ikakopa1985 <57399769+ikakopa1985@users.noreply.github.com>
2026-09-25 19:20:41 +0400

    სინქრონიზაცია

--- gymapp/management/commands/update_status.py
+++ gymapp/management/commands/update_status.py
@@ -1,9 +1,8 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
-from django.db.models import Q
+from django.db.models import Q, F
 
-from gymapp.models import ClientMembership
-from gymapp.models import ClientSync
+from gymapp.models import ClientMembership,ClientSync,RevisionNumber
 
 
 
@@ -12,72 +11,82 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
 
-        today = timezone.localdate()
+        revision = RevisionNumber.get_revision()
 
-        updated = 0
-
-        cms = ClientMembership.objects.select_related("membership").filter(
-            status="active"
-        ).filter(
-            Q(start_date__gt=today) |
-            Q(end_date__lt=today)
+        RevisionNumber.objects.filter(pk=revision.pk).update(
+            local_revizion_number=F("local_revizion_number") + 1
         )
 
-        for cm in cms:
+        # today = timezone.localdate()
+        #
+        # updated = 0
+        #
+        # cms = ClientMembership.objects.select_related("membership").filter(
+        #     status="active"
+        # ).filter(
+        #     Q(start_date__gt=today) |
+        #     Q(end_date__lt=today)
+        # )
+        #
+        # for cm in cms:
+        #
+        #     expired = False
+        #
+        #     if cm.membership.membership_type == "fixed":
+        #             expired = True
+        #
+        #     if expired:
+        #
+        #         cm.status = "expired"
+        #         cm.save(update_fields=["status"])
+        #
+        #         # ZKT-დან წასაშლელი task
+        #         exists = ClientSync.objects.filter(
+        #             client=cm.client,
+        #             action="delete",
+        #             status="pending"
+        #         ).exists()
+        #
+        #         if not exists:
+        #             ClientSync.objects.create(
+        #                 client=cm.client,
+        #                 action="delete",
+        #                 status="pending"
+        #             )
+        #
+        #         updated += 1
+        #
+        # cms_to_activate = ClientMembership.objects.select_related("membership").filter(
+        #     status__in=["expired", "paused"],
+        #     start_date__lte=today,
+        #     end_date__gte=today
+        # )
+        #
+        # for cm in cms_to_activate:
+        #
+        #     cm.status = "active"
+        #     cm.save(update_fields=["status"])
+        #
+        #     # ZKT-დან წასაშლელი task
+        #     exists = ClientSync.objects.filter(
+        #         client=cm.client,
+        #         action="add",
+        #         status="pending"
+        #     ).exists()
+        #
+        #     if not exists:
+        #         ClientSync.objects.create(
+        #             client=cm.client,
+        #             action="add",
+        #             status="pending"
+        #         )
+        #
+        #     updated += 1
+        #
+        # self.stdout.write(
+        #     self.style.SUCCESS(f"შემოწმდა {cms.count()} აბონემენტი. დაკორექტირდა  {updated}")
 
-            expired = False
-
-            if cm.membership.membership_type == "fixed":
-                    expired = True
-
-            if expired:
-
-                cm.status = "expired"
-                cm.save(update_fields=["status"])
-
-                # ZKT-დან წასაშლელი task
-                exists = ClientSync.objects.filter(
-                    client=cm.client,
-                    action="delete",
-                    status="pending"
-                ).exists()
-
-                if not exists:
-                    ClientSync.objects.create(
-                        client=cm.client,
-                        action="delete",
-                        status="pending"
-                    )
-
-                updated += 1
-
-        cms_to_activate = ClientMembership.objects.select_related("membership").filter(
-            status__in=["expired", "paused"],
-            start_date__lte=today,
-            end_date__gte=today
-        )
-
-        for cm in cms_to_activate:
-
-            cm.status = "active"
-            cm.save(update_fields=["status"])
-
-            # ZKT-დან წასაშლელი task
-            exists = ClientSync.objects.filter(
-                client=cm.client,
-                action="add",
-                status="pending"
-            ).exists()
-
-            if not exists:
-                ClientSync.objects.create(
-                    client=cm.client,
-                    action="add",
-                    status="pending"
-                )
-
-            updated += 1
 
         self.stdout.write(
-            self.style.SUCCESS(f"შემოწმდა {cms.count()} აბონემენტი. დაკორექტირდა  {updated}")
+            self.style.SUCCESS(f"გაკეთდა  რევიზია")
         )
